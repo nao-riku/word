a227208d5525603b479deecf2b2bfef8ae6e6484
nao-riku <76162238+nao-riku@users.noreply.github.com>
2022-11-03 10:26:41 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.onresize = resize;
 
 function resize() {
     document.getElementsByClassName("question")[0].style.height = (document.documentElement.clientHeight - 200) + "px";
-    document.getElementsByClassName("question")[0].style.width = "100%";
+    document.body.style.width = "100%";
 }
 
 function add_onclick(a, b) {
